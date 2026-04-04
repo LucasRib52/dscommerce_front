266c049e3881fa4c90214cf5a29c2ccea151ef0c
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-04-04 17:27:01 -0300

    Detalhes do produto dinâmico com parâmetro de rota

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import type { ProductDTO } from "../../models/product";
+import { Link } from "react-router-dom";
 
 type Props = {
   product: ProductDTO;
@@ -7,15 +8,17 @@ type Props = {
 
 function CatalogCard({ product }: Props) {
   return (
-    <div className="dsc-card">
-      <div className="dsc-catalog-card-top dsc-line-bottom">
-        <img src={product.imgUrl} alt="Computer" />
+    <Link to={`/product-details/${product.id}`}>
+      <div className="dsc-card">
+        <div className="dsc-catalog-card-top dsc-line-bottom">
+          <img src={product.imgUrl} alt="Computer" />
+        </div>
+        <div className="dsc-catalog-card-bottom">
+          <h3>R$ {product.price.toFixed(2)}</h3>
+          <h4>{product.name}</h4>
+        </div>
       </div>
-      <div className="dsc-catalog-card-bottom">
-        <h3>R$ {product.price.toFixed(2)}</h3>
-        <h4>{product.name}</h4>
-      </div>
-    </div>
+    </Link>
   );
 }
 
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,33 +1,20 @@
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import type { ProductDTO } from "../../../models/product";
 import "./styles.css";
-
-const product: ProductDTO = {
-  id: 2,
-  name: "Smart TV",
-  description: "Esta TV é muito bonita",
-  price: 2500.99,
-  imgUrl:
-    "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
-  categories: [
-    {
-      id: 2,
-      name: "Eletronicos",
-    },
-    {
-      id: 3,
-      name: "Computadores",
-    },
-  ],
-};
+import * as productService from "../../../services/product-service";
+import { useParams } from "react-router-dom";
 
 function ProductDetails() {
+  const params = useParams();
+
+  const product = productService.findById(Number(params.productId));
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        <ProductDetailsCard product={product} />
+        {product && <ProductDetailsCard product={product} />}
+
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
           <ButtonInverse text="Inicio" />

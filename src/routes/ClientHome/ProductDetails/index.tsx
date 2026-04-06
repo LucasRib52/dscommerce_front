f01259b3dc41677410bd680e6177a85441df0d9b
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-04-06 05:38:32 -0300

    Requisições para o catálogo e no productDetails, com axios

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,13 +2,23 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import "./styles.css";
-import * as productService from "../../../services/product-service";
 import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { ProductDTO } from "../../../models/product";
+import axios from "axios";
 
 function ProductDetails() {
   const params = useParams();
 
-  const product = productService.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/products/${params.productId}`)
+      .then((response) => {
+        setProduct(response.data);
+      });
+  });
 
   return (
     <main>

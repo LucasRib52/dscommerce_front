76abdee232c2810c6d08753a9e6e8a068bd52da1
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-04-13 06:37:43 -0300

    conseguindo adcionar um intem ao carrinho quando clicamos em comprar

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -6,6 +6,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
+import * as cartService from "../../../services/cart-service";
 
 function ProductDetails() {
   const params = useParams();
@@ -25,13 +26,23 @@ function ProductDetails() {
       });
   });
 
+  function handleBuyClick() {
+    if (product) {
+      cartService.addProduct(product);
+      navigate("/cart");
+    }
+  }
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
         {product && <ProductDetailsCard product={product} />}
 
         <div className="dsc-btn-page-container">
-          <ButtonPrimary text="Comprar" />
+          <div onClick={handleBuyClick}>
+            <ButtonPrimary text="Comprar" />
+          </div>
+
           <Link to={"/"}>
             <ButtonInverse text="Inicio" />
           </Link>

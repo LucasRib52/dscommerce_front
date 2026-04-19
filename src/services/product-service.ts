969c1b502a4a56601a46b519869391ed15378d17
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-04-19 15:07:06 -0300

    Criando o método auxiliar requestBackend

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,5 +1,6 @@
 import axios, { type AxiosRequestConfig } from "axios";
 import { BASE_URL } from "../utils/system";
+import { requestBackend } from "../utils/requests";
 
 export function findPageRequest(
   page: number,
@@ -9,7 +10,6 @@ export function findPageRequest(
 ) {
   const config: AxiosRequestConfig = {
     method: "GET",
-    baseURL: BASE_URL,
     url: "/products",
     params: {
       page,
@@ -19,9 +19,9 @@ export function findPageRequest(
     },
   };
 
-  return axios(config);
+  return requestBackend(config);
 }
 
 export function findById(id: number) {
-  return axios.get(`${BASE_URL}/products/${id}`);
+  return requestBackend({ url: `/products/${id}` });
 }

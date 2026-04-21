282b0ed21be2ad4fd827b9111a041bf1c733a652
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-04-21 17:22:13 -0300

    Registrando pedido e fazendo ajustes finais

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,14 @@ function App() {
               />
               <Route path="cart" element={<Cart />} />
               <Route path="login" element={<Login />} />
-              <Route path="confirmation/:orderId" element={<Confirmation />} />
+              <Route
+                path="confirmation/:orderId"
+                element={
+                  <PrivateRoute>
+                    <Confirmation />
+                  </PrivateRoute>
+                }
+              />
             </Route>
             <Route
               path="/admin/"
